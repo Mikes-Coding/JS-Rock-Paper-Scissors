61eecf9983d566e626899dba347f7d51558b1687
Michael Ross <michael.ross.contact@gmail.com>
2023-03-10 06:50:44 +0000

    Update javascript.js
    Added playRound function to compare the player and computer choice

--- javascript.js
+++ javascript.js
@@ -1,7 +1,36 @@
 function getComputerChoice() {
-    const options = ['rock', 'paper', 'scissor'];
+    const options = ['rock', 'paper', 'scissors'];
     const randomIndex = Math.floor(Math.random() * options.length);
     return options[randomIndex];
   }
+
+  function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+      return "It's a tie!";
+    } else if (playerSelection === "rock") {
+      if (computerSelection === "scissors") {
+        return "Player 1 wins!";
+      } else {
+        return "Player 2 wins!";
+      }
+    } else if (playerSelection === "paper") {
+      if (computerSelection === "rock") {
+        return "Player 1 wins!";
+      } else {
+        return "Player 2 wins!";
+      }
+    } else if (playerSelection === "scissors") {
+      if (computerSelection === "paper") {
+        return "Player 1 wins!";
+      } else {
+        return "Player 2 wins!";
+      }
+    } else {
+      return "Invalid input. Please enter 'rock', 'paper', or 'scissors'.";
+    }
+  }
+   
+  const playerSelection = "rock";
+  const computerSelection = getComputerChoice();
   
-  getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
