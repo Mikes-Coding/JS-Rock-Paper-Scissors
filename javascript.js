4e14d64b74c392660a3a05283246aaac6f7183d0
Michael Ross <michael.ross.contact@gmail.com>
2023-04-02 17:56:16 +0100

    Completed the UI and the game ends after 5 wins

--- javascript.js
+++ javascript.js
@@ -2,27 +2,35 @@ const scissorButton = document.getElementById("scissorButton");
 const paperButton = document.getElementById("paperButton");
 const rockButton = document.getElementById("rockButton");
 
+const pageresultElement = document.getElementById("result");
+const scoreElement = document.getElementById("score");
+
 let playerSelection;
+const tally = document.getElementById("tally");
+
+let playerScore = 0;
+let computerScore = 0;
 
 scissorButton.addEventListener("click", function() {
   playerSelection = scissorButton.value;
   const computerSelection = getComputerChoice();
-  const result = playRound(playerSelection, computerSelection);
-  console.log(result);
+  /* playRound(playerSelection, computerSelection); */
+  game();
+  updateScore();
 });
 
 paperButton.addEventListener("click", function() {
   playerSelection = paperButton.value;
   const computerSelection = getComputerChoice();
-  const result = playRound(playerSelection, computerSelection);
-  console.log(result);
+  game();
+  updateScore();
 });
 
 rockButton.addEventListener("click", function() {
   playerSelection = rockButton.value;
   const computerSelection = getComputerChoice();
-  const result = playRound(playerSelection, computerSelection);
-  console.log(result);
+  game();
+  updateScore();
 });
 
 function getComputerChoice() {
@@ -33,57 +41,81 @@ function getComputerChoice() {
 
   function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-      return "It's a tie!";
+      pageresultElement.innerHTML = "It's a tie!";
     } else if (playerSelection === "rock") {
       if (computerSelection === "scissors") {
-        return "Player 1 wins!";
+        pageresultElement.innerHTML = "You win! Rock beats scissors.";
+        playerScore++;
       } else {
-        return "Player 2 wins!";
+        pageresultElement.innerHTML = "You lose! Paper beats rock.";
+        computerScore++;
       }
     } else if (playerSelection === "paper") {
       if (computerSelection === "rock") {
-        return "Player 1 wins!";
+        pageresultElement.innerHTML = "You win! Paper beats rock.";
+        playerScore++;
       } else {
-        return "Player 2 wins!";
+        pageresultElement.innerHTML = "You lose! Scissors beats paper.";
+        computerScore++;
       }
     } else if (playerSelection === "scissors") {
       if (computerSelection === "paper") {
-        return "Player 1 wins!";
+        pageresultElement.innerHTML = "You win! Scissors beats paper.";
+        playerScore++;
       } else {
-        return "Player 2 wins!";
+        pageresultElement.innerHTML = "You lose! Rock beats scissors.";
+        computerScore++;
       }
     } else {
-      return "Invalid input. Please enter 'rock', 'paper', or 'scissors'.";
+      pageresultElement.innerHTML = "Invalid input. Please enter 'rock', 'paper', or 'scissors'.";
     }
+  }
+  
+  function updateScore() {
+    scoreElement.innerHTML = "Player: " + playerScore + " Computer: " + computerScore;
   }
 
   function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-  
-
-    /*
-    for (let i = 0; i < 5; i++) {
-      const playerSelection = prompt("Pick rock, paper or scissors");
+    let winner = "";
+    for (let i = 0; i < 1; i++) { // play only one round
       const computerSelection = getComputerChoice();
-  
       const result = playRound(playerSelection, computerSelection);
-      console.log(result);
   
-      if (result === "Player 1 wins!") {
+      if (result === "player") {
         playerScore++;
-      } else if (result === "Player 2 wins!") {
+        winner = "Player 1";
+      } else if (result === "computer") {
         computerScore++;
+        winner = "Player 2";
+      } else {
+        winner = "Tie";
+      }
+  
+      tally.innerHTML = "Player 1: " + playerScore + " | Player 2: " + computerScore;
+  
+      if (playerScore === 5 || computerScore === 5) { // check for winner
+        if (playerScore === 5) {
+          pageresultElement.innerHTML = "Player 1 wins!";
+        } else {
+          pageresultElement.innerHTML = "Player 2 wins!";
+        }
+  
+        setTimeout(function() { // reset scores after a delay of 3 seconds
+          playerScore = 0;
+          computerScore = 0;
+          tally.innerHTML = "Player 1: " + playerScore + " | Player 2: " + computerScore;
+          pageresultElement.innerHTML = "";
+        }, 3000);
+  
+        break; // exit the loop if there is a winner
       }
     }
-  */
-
-    console.log("Final score:");
-    console.log("Player: " + playerScore);
-    console.log("Computer: " + computerScore);
-  
-    return { player: playerScore, computer: computerScore };
   }
   
-  game();
+  
+  
+  
+  
+  
+  
   
